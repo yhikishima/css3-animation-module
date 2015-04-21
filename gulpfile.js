3096ecc02853785a219daaa8d908cc7a88298108
yasuaki-hikishima <yasuaki_hikishima@waku-2.com>
2015-04-21 09:11:51 +0900

    add stylus plugin and autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -6,17 +6,29 @@ var watch = require('gulp-watch');
 var webserver = require('gulp-webserver');
 
 gulp.task('stylus', function () {
-  gulp.src('src/stylus/*.styl')
+  gulp.src([
+    'src/stylus/*.styl',
+    '!' + 'src/stylus/_*.styl'
+    ])
     .pipe(plumber())
-    .pipe(stylus())
+    .pipe(stylus({
+      use: [autoprefixer()]
+    }))
     .pipe(gulp.dest('dist/css/'));
 });
 
+gulp.task('copy', function() {
+  return gulp.src([
+    'src/*.html'
+  ])
+  .pipe( gulp.dest('dist/'));
+});
+
 gulp.task('webserver', function() {
-  gulp.src('app')
+  gulp.src('dist')
     .pipe(webserver({
-        port: 9000,
-        livereload: true
+      port: 9000,
+      livereload: true
     }));
 });
 
@@ -24,10 +36,16 @@ gulp.task('watch', function() {
   watch('src/stylus/*.styl', function () {
     gulp.start(['stylus']);
   });
+
+  watch('src/*.html', function () {
+    gulp.start(['copy']);
+  });
+
 });
 
 gulp.task('serve', [
   'stylus',
   'webserver',
+  'copy',
   'watch'
 ]);
